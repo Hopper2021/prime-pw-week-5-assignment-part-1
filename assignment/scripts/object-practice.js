462813acb12746ead05aa5a0e993501fae00d724
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-10 14:50:31 -0500

    Logged me.shoeCount and verified in console

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -45,6 +45,8 @@ console.log( me.favThreeFoods[2] );
   - Console.log your updated shoe count.
 */
 
+console.log( me.shoeCount );
+
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
   - Do this the same way you updated the variable above
